a5e116161034e9ac5385d124032aa56395988781
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-29 01:07:23 +0600

    header section completed

---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css'
+import { Link } from 'react-router-dom';
+import logo from '../../logo.jpg'
+
+const Header = () => {
+    return (
+        <div className='d-flex justify-content-between container'>
+            <div>
+                <Link to='/'>
+                    <img className='w-75' src={logo} alt="" />
+                </Link>
+            </div>
+            <div className='mt-4 links'>
+                <Link className='mx-4 text-decoration-none' to='/'>HOME</Link>
+                <Link className='mx-4 text-decoration-none' to='/inventory'>INVERTORY</Link>
+                <Link className='mx-4 text-decoration-none' to='/blogs'>BLOGS</Link>
+                <Link className='mx-4 text-decoration-none' to='/'>HOME</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
