9165a8e8cff05657274657cdbc97adecaebce31c
zillurahman10 <raihan.brac2019@gmail.com>
2022-04-30 04:57:04 +0600

    login form some style nade

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import './Login.css'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
-    return (
-        <div>
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const navigate = useNavigate()
 
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        signInWithEmailAndPassword(email, password)
+
+        console.log(email, password);
+    }
+
+    if (user) {
+        navigate('/')
+    }
+    return (
+        <div className='form-div'>
+            <div className='d-flex justify-content-center mt-5'>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <h2 className='text-center form-title'>Please Login</h2>
+                    <input className='email-input' type="email" name="email" id="" placeholder='Email' />
+                    <br />
+                    <input className='password-input' type="password" name="password" id="" placeholder='Password' />
+                    <br />
+                    <input className='submit-button' type="submit" value="Login" />
+                    <p className='text-center mt-3 form-link'>New in Car.com ? <Link className='text-decoration-none form-link' to='/signup'>Create a new account</Link></p>
+                    <div className='text-center d-flex'>
+                        <div style={{ border: '1px solid gray', height: '1px' }}>
+
+                        </div>
+                        <p>or</p>
+                        <div>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init'
 
 const SignUp = () => {
@@ -9,16 +10,26 @@ const SignUp = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate()
 
     const handleSubmit = e => {
         e.preventDefault()
 
         const name = e.target.name.value
         const email = e.target.email.value
-        const password = e.target.value
+        const password = e.target.password.value
         const confirmPassword = e.target.confirmPassword.value
 
         createUserWithEmailAndPassword(email, password)
+
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        console.log(confirmPassword);
+
+    }
+    if (user) {
+        navigate('/')
     }
     return (
         <div>
@@ -33,6 +44,7 @@ const SignUp = () => {
                 <input type="password" name="confirmPassword" id="" placeholder='Confirm Password' />
                 <br />
                 <input type="submit" value="Sign Up" />
+                <p>Already have an account ? <Link to='/login'>Login</Link></p>
             </form>
         </div>
     );
