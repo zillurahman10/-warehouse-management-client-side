695508bfa3db49a393cf4777becd9cc9e2382460
zillurahman10 <raihan.brac2019@gmail.com>
2022-05-02 00:13:50 +0600

    no work done but put has implemented

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -10,6 +10,25 @@ const Inventory = () => {
             .then(data => setProduct(data))
     }, [id])
     const { name, img, price, description, pastMileage, fuelType, cc, quantity, _id, supplierName } = product
+    const [productQuantity, setProductQuantity] = useState("")
+    const handleDeliver = () => {
+        const count = (quantity - 1).toString()
+        const update = { count }
+        const url = `http://localhost:5000/inventory/${_id}`
+        console.log(url);
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(update)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(parseInt(data.count));
+                setProductQuantity(parseInt(data.count))
+            })
+    }
     return (
         <div>
             <div className='d-flex justify-content-center mt-5 shadow-lg mx-auto p-5 rounded-3' style={{ width: '800px' }}>
@@ -19,10 +38,10 @@ const Inventory = () => {
                 <div>
                     <h4>{name}</h4>
                     <p>{description}</p>
-                    <p>Quantity : {quantity}</p>
+                    <p>Quantity : {productQuantity}</p>
                     <p>Supplier : {supplierName}</p>
                     <h5>{price}</h5>
-                    <button>Update</button>
+                    <button onClick={handleDeliver}>Update</button>
                 </div>
             </div>
             <div className='mt-5 d-flex justify-content-center'>
