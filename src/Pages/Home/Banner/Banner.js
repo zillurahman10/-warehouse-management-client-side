641f42a6017526140d5dc40e899dab3988afae23
zilurahman10 <raihan.brac2019@gmail.com>
2022-05-04 00:18:29 +0600

    product card more style added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,10 @@ const Banner = () => {
     return (
         <div>
             <div className='banner'>
-                <h1 data-aos="fade-up" data-aos-duration="1000" className='text-center align-items-center mt-5'>Hello</h1>
+                <div data-aos="fade-up" data-aos-duration="1000">
+                    <h1 className='text-center align-items-center mt-5'>Hello</h1>
+                    <p className='text-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, ipsa id? Temporibus a minus labore tempora nobis eum. Magnam aliquid quas quidem soluta nostrum facere excepturi quisquam? Sed modi vero nemo necessitatibus nobis neque. Nemo eius iure at quia rerum repellendus, quibusdam natus id fugit fuga aspernatur minima placeat tempore.</p>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Product.css'
-import { faRoad, faGasPump, } from '@fortawesome/free-solid-svg-icons'
+import { faRoad, faGasPump } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import engineIcon from '../../../images/engineIcon.png'
 
 const Product = ({ product }) => {
     const { name, img, price, description, pastMileage, fuelType, cc, quantity, _id, supplierName } = product
@@ -17,18 +18,19 @@ const Product = ({ product }) => {
             </div>
             <div>
                 <h4 className='mt-3'>{name}</h4>
-                <FontAwesomeIcon className='mx-2' icon={faRoad}></FontAwesomeIcon>
-                <span>{pastMileage}</span>
-                <FontAwesomeIcon className='mx-2' icon={faGasPump}></FontAwesomeIcon>
+                <FontAwesomeIcon className='mx-1' icon={faRoad}></FontAwesomeIcon>
+                <span className='me-2 mt-3'>{pastMileage}</span>
+                <FontAwesomeIcon className='mx-1' icon={faGasPump}></FontAwesomeIcon>
                 <span>{fuelType}</span>
+                <img className='engineIcon' src={engineIcon} alt="" />
                 <span>{cc}</span>
-                <p>Supplier : {supplierName}</p>
+                <p className='mt-3'>Supplier : {supplierName}</p>
                 <p>Quantity : {quantity}</p>
                 <p>{description}</p>
             </div>
             <div>
-                <h3>$ {price}</h3>
-                <button onClick={handleInventory}>Update</button>
+                <h5>$ {price}</h5>
+                <button className='update-button' onClick={handleInventory}>Update</button>
             </div>
         </div>
     );
