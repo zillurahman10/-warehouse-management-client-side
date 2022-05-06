41d0edbd017a6acfc836348833bb9b48a8e724b5
zilurahman10 <raihan.brac2019@gmail.com>
2022-05-06 23:49:36 +0600

    increase quantity work not completed

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const Inventory = () => {
-    const { id } = useParams()
     const [product, setProduct] = useState({})
+    const [productQuantity, setProductQuantity] = useState(Number)
+    const [reload, setIsReload] = useState(true)
+    const [increaseQuantity, setIncreaseQuantity] = useState(Number)
+    const [updatedQuantity, setUpdatedQuantity] = useState(Number)
+    const { id } = useParams()
+
     useEffect(() => {
         fetch(`http://localhost:5000/inventory/${id}`)
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [id])
     const { name, img, price, description, pastMileage, fuelType, cc, quantity, _id, supplierName } = product
-    const [productQuantity, setProductQuantity] = useState()
 
 
     const handleDeliver = () => {
-        const count = parseInt(quantity - 1)
-        const update = { count, product }
+        const minusQuantity = quantity - 1
+        const update = { minusQuantity, product }
         const url = `http://localhost:5000/inventory/${_id}`
         fetch(url, {
             method: "PUT",
@@ -28,11 +32,37 @@ const Inventory = () => {
             .then(data => {
                 console.log(data);
                 alert('updated')
-                setProductQuantity(count)
+                setProductQuantity(minusQuantity)
             })
+        setIsReload(!reload)
     }
+
     console.log(quantity);
     console.log(productQuantity);
+    const handleQuantity = e => {
+        setIncreaseQuantity(e.target.value)
+    }
+    const updateQuantity = e => {
+        e.preventDefault()
+        const plusQuantity = quantity + increaseQuantity
+        const update = { plusQuantity, product }
+        const url = `http://localhost:5000/inventory/${_id}`
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(update)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                alert('updated')
+                setUpdatedQuantity(plusQuantity)
+            })
+        setIsReload(!reload)
+    }
+    console.log(updatedQuantity);
     return (
         <div>
             <div className='d-flex justify-content-center mt-5 shadow-lg mx-auto p-5 rounded-3' style={{ width: '800px' }}>
@@ -42,7 +72,7 @@ const Inventory = () => {
                 <div>
                     <h4>{name}</h4>
                     <p>{description}</p>
-                    <p>Quantity : {productQuantity ? productQuantity : quantity}</p>
+                    <p>Quantity : {updatedQuantity || productQuantity || quantity}</p>
                     <p>Supplier : {supplierName}</p>
                     <h5>Price : {price}</h5>
                     <button onClick={handleDeliver}>Update</button>
@@ -50,6 +80,13 @@ const Inventory = () => {
             </div>
             <div className='mt-5 d-flex justify-content-center'>
                 <Link to='/manageinventory' className='btn btn-primary'>Manage Inventories</Link>
+            </div>
+            <div>
+                <form onSubmit={updateQuantity}>
+                    <input onBlur={handleQuantity} type="number" name="quantity" id="" placeholder='Product Quantity' required />
+                    <br />
+                    <input type="submit" value="increase" />
+                </form>
             </div>
         </div>
     );
